44f566ad5d575d7bfee976d502e8d5ce8f8ea8a0
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-05-09 23:26:49 -0600

    se agrego la clase service udpate

--- src/main/java/com/luis/curso/springboot/app/springbootcrud/services/ProductService.java
+++ src/main/java/com/luis/curso/springboot/app/springbootcrud/services/ProductService.java
@@ -12,5 +12,7 @@ public interface ProductService {
 
     Product save(Product product);
 
-    public Optional<Product> delete(Product product);
+    Optional<Product> update(Long id, Product product);
+
+    public Optional<Product> delete(Long id);
 }
--- src/main/java/com/luis/curso/springboot/app/springbootcrud/services/ProductServiceImpl.java
+++ src/main/java/com/luis/curso/springboot/app/springbootcrud/services/ProductServiceImpl.java
@@ -32,11 +32,27 @@ public class ProductServiceImpl implements ProductService{
         return repository.save(product);
     }
 
+    @Transactional
     @Override
-    public Optional<Product> delete(Product product) {
-        Optional<Product> productOptional = repository.findById(product.getId());
+    public Optional<Product> update(Long id, Product product) {
+        Optional<Product> productOptional = repository.findById(id);
+        if(productOptional.isPresent()) {
+            Product productDb = productOptional.orElseThrow();
+            productDb.setName((product.getName()));
+            productDb.setDescription(product.getDescription());
+            productDb.setPrice(product.getPrice());
+            return Optional.of(repository.save(productDb));
+
+        };
+        return productOptional;
+    }
+
+    @Transactional
+    @Override
+    public Optional<Product> delete(Long id) {
+        Optional<Product> productOptional = repository.findById(id);
         productOptional.ifPresent(productDb -> {
-            repository.delete(product);
+            repository.delete(productDb);
         });
         return productOptional;
     }
